8f0842ea2cd9ebfffaf2ec5957c6ccffca3dd762
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2022-12-18 14:10:45 +0100

    added previos 1 2 3 next

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+import Pagination from "./components/Pagination/Pagination";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const lastPage = 20;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>React TypeScript Pagination</h1>
+      <Pagination
+        currentPage={currentPage}
+        lastPage={lastPage}
+        maxLength={7}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 }
